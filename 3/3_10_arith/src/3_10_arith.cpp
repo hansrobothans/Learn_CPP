5ece9472adbf2c1b0cfd47f47f455a8b05b9cafd
hansrobothans <1245384082@qq.com>
2024-02-05 17:32:51 +0800

    增加：运算符和数组

---
+++ 3/3_10_arith/src/3_10_arith.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    float hats, heads;
+    /*
+    cout默认会删除结尾0，调用cout.setf()会覆盖这个行为
+    调用cout.setf(ios_base::fixed, ios_base::floatfield)会保留小数点后的0
+    */
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    cout << "Enter a number: ";
+    cin >> hats;
+    cout << "Enter another number: ";
+    cin >> heads;
+
+    cout << "hats = " << hats << ", heads = " << heads << endl;
+    cout << "hats +  heads = " << hats + heads << endl;
+    cout << "hats -  heads = " << hats - heads << endl;
+    cout << "hats *  heads = " << hats * heads << endl;
+    cout << "hats /  heads = " << hats / heads << endl;
+    return 0;
+}
