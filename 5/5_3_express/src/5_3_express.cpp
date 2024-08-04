5fe1ef2c90e43bafeaa933091852665e02b38b30
hansrobothans <1245384082@qq.com>
2024-08-04 21:11:42 +0800

    增加：新增for循环

---
+++ 5/5_3_express/src/5_3_express.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int x;
+
+    cout << "The expression x = 100 has the value ";
+    cout << (x = 100) << endl;
+    cout << "Now x = " << x << endl;
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+
+    cout.setf(ios_base::boolalpha); //C++新特点
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    cout << 10 << endl;
+    return 0;
+}
